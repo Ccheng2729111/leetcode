64597a7388e99d979bfd8b136daea96b76c88a5b
kian <Bkcheng2729111@163.com>
2023-08-28 16:01:08 +0800

    chore

--- src/kian&2023-08-28/insert.js
+++ src/kian&2023-08-28/insert.js
@@ -5,13 +5,13 @@
  */
 
 // #57
-const intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-const newInterval = [4, 8];
+const intervals = [[1,5]];
+const newInterval = [6, 8];
 var insert = function (intervals, newInterval) {
   intervals.push(newInterval);
-  let list = intervals.sort((a, b) => a[0] - b[0])
+  let list = intervals.sort((a, b) => a[0] - b[0]);
 
-  let result = []
+  let result = [];
 
   let prev = list[0];
 
@@ -21,13 +21,44 @@ var insert = function (intervals, newInterval) {
       prev[1] = Math.max(prev[1], cur[1]);
     } else {
       result.push(prev);
-      prev = cur
+      prev = cur;
     }
   }
 
-  result.push(prev)
+  result.push(prev);
 
-  return result
+  return result;
 };
 
-insert(intervals, newInterval);
+var insert1 = function (intervals, newInterval) {
+  let result = [];
+
+  let size = intervals.length;
+  let i = 0;
+
+  // 处理左边界 如果当前遍历的右边界小于目标的左边界 则符合
+  while (i < size && intervals[i][1] < newInterval[0]) {
+    result.push(intervals[i++]);
+  }
+
+  // 这里需要注意 因为本身的排序已经按照左边界排过了 所以这里交集的左边界已经可以确认为目标和当前遍历左边界的最小值
+  // 然后判断遍历的左边界是否小于目标的右边界，如果是的，那么需要更新目标右边界为两者的右边界的最大值
+  // 一直到遍历的左边界大于目标的右边界，最后推到结果中
+  if (i < size) {
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+    while (i < size && intervals[i][0] <= newInterval[1]) {
+      newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+      i++;
+    }
+  }
+  result.push(newInterval);
+
+  // 处理右边没有重叠的部分
+  while (i < size && intervals[i][0] > newInterval[1]) {
+    result.push(intervals[i++]);
+  }
+
+  return result;
+};
+
+insert1(intervals, newInterval);
